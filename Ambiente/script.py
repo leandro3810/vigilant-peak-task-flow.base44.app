430c8389517e4dad33f3a4766aee3ad4ca031cb2
Leandro Ribeiro Gomes <ledielsong9@gmail.com>
2025-11-18 18:44:54 -0300

    Update Ambiente/script.py
    Co-authored-by: sourcery-ai[bot] <58596630+sourcery-ai[bot]@users.noreply.github.com>
    Signed-off-by: Leandro Ribeiro Gomes   <ledielsong9@gmail.com>

--- Ambiente/script.py
+++ Ambiente/script.py
@@ -211,7 +211,7 @@ def run_setup (script_name, script_args=None, stop_after="run"):
             sys.argv[0] = script_name
             if script_args is not None:
                 sys.argv[1:] = script_args
-            with open(script_name, 'rb') as f:
+            with open(script_name, 'r', encoding='utf-8') as f:
                 exec(f.read(), g)
         finally:
             sys.argv = save_argv

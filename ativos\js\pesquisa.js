52a4af1230f002b6285800f6b7111ec5532dc7a4
Leandro Ribeiro Gomes <ledielsong9@gmail.com>
2026-04-02 13:02:03 -0300

    Delete ativos\js\pesquisa.js
    Signed-off-by: Leandro Ribeiro Gomes   <ledielsong9@gmail.com>

--- ativos\js\pesquisa.js
+++
@@ -1,9 +0,0 @@
-// Exemplo simples de busca local
-document.addEventListener('DOMContentLoaded', () => {
-  const params = new URLSearchParams(window.location.search);
-  const query = params.get('q');
-  if (query) {
-    document.getElementById('search-results').innerText = `Resultados para "${query}": (implemente aqui a busca real)`;
-    // Aqui você pode conectar o Lunr.js ou Algolia
-  }
-});

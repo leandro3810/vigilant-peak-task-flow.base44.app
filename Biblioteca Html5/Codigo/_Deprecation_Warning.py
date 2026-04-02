98ab89d61a45429c45511c34953581e0c2a1a446
Leandro Ribeiro Gomes <ledielsong9@gmail.com>
2026-04-02 12:27:16 -0300

    Delete Biblioteca Html5/Codigo directory
    Signed-off-by: Leandro Ribeiro Gomes   <ledielsong9@gmail.com>

--- Biblioteca Html5/Codigo/_Deprecation_Warning.py
+++
@@ -1,7 +0,0 @@
-class SetuptoolsDeprecationWarning(Warning):
-    """
-    Base class for warning deprecations in ``setu
-
-    This class is not derived from ``DeprecationW
-    visible by default.
-    """

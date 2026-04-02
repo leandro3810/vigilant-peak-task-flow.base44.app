66c726c802ba08ece334c293d2f9d92bed8b4636
Leandro Ribeiro Gomes <ledielsong9@gmail.com>
2026-04-02 12:31:49 -0300

    Delete Estrutura inicial da Biblioteca directory
    Signed-off-by: Leandro Ribeiro Gomes   <ledielsong9@gmail.com>

--- Estrutura inicial da Biblioteca/Tests animations.test.js
+++
@@ -1,8 +0,0 @@
-import { fadeIn, fadeOut } from '../src/animations';
-
-test('fadeIn applies correct styles', () => {
-  const element = document.createElement('div');
-  fadeIn(element, 1000);
-  expect(element.style.transition).toBe('opacity 1000ms');
-  expect(element.style.opacity).toBe('1');
-});
